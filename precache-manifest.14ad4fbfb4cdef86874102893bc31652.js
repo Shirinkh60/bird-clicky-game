430933ab1a1e58a0c14f4d22811e4ed8cf5932f9
Shirin <shirin.boroujeni@gmail.com>
2018-10-24 20:36:32 -0700

    Updates

---
+++ precache-manifest.14ad4fbfb4cdef86874102893bc31652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a0f7d40ea5e4423abde53f16253d3e5",
+    "url": "/bird-clicky-game/static/media/title.9a0f7d40.jpg"
+  },
+  {
+    "revision": "dcd3105dedd8df41dac5",
+    "url": "/bird-clicky-game/static/js/runtime~main.dcd3105d.js"
+  },
+  {
+    "revision": "217e211548b0b6a68bd0",
+    "url": "/bird-clicky-game/static/js/main.217e2115.chunk.js"
+  },
+  {
+    "revision": "d7c3259394c4b01131ba",
+    "url": "/bird-clicky-game/static/js/1.d7c32593.chunk.js"
+  },
+  {
+    "revision": "217e211548b0b6a68bd0",
+    "url": "/bird-clicky-game/static/css/main.3687acaf.chunk.css"
+  },
+  {
+    "revision": "453a2441c960ac3dd283520aadfda5ca",
+    "url": "/bird-clicky-game/index.html"
+  }
+];
